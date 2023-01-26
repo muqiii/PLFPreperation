574cf081d4128c2a10c817e65e4364a46c7dfbd0
Matteo Martinz <80289094+muqiii@users.noreply.github.com>
2023-01-26 09:54:42 +0100

    Initial commit

---
+++ src/main/java/com/example/plfpreperation/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.plfpreperation;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
